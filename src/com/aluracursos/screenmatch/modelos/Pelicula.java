c1a98a9e6d078dcb563644425cafa85445488ead
Daniel Romero <dandres508@gmail.com>
2024-09-19 17:16:51 -0500

    added package serie and titulo

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,63 +1,14 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
+public class Pelicula extends Titulo {
 
-    private String nombre;
-    private int fechaLanzamineto;
-    private int duracionMinutos;
-    private boolean incluidoPlan;
-    private double sumaEvaluaciones;
-    private int totalEvaluciones;
+    private String director;
 
-    public String getNombre() {
-        return nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public int getFechaLanzamineto() {
-        return fechaLanzamineto;
-    }
-
-    public int getDuracionMinutos() {
-        return duracionMinutos;
-    }
-
-    public boolean isIncluidoPlan() {
-        return incluidoPlan;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setFechaLanzamineto(int fechaLanzamineto) {
-        this.fechaLanzamineto = fechaLanzamineto;
-    }
-
-    public void setDuracionMinutos(int duracionMinutos) {
-        this.duracionMinutos = duracionMinutos;
-    }
-
-    public void setIncluidoPlan(boolean incluidoPlan) {
-        this.incluidoPlan = incluidoPlan;
-    }
-
-    public int getTotalEvaluciones(){
-        return totalEvaluciones;
-    }
-
-
-    public void muestraFichaTecnica() {
-        System.out.println("El nombre de la película es: " + nombre);
-        System.out.println("La película Fue estrena en el año: " + fechaLanzamineto);
-        System.out.println("La duracion en minutos de la película es: " + duracionMinutos);
-    }
-
-    public void evaluacion(double nota) {
-        sumaEvaluaciones += nota;
-        totalEvaluciones++;
-    }
-
-    public double calculaMedia() {
-        return sumaEvaluaciones / totalEvaluciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
