5f1aa3c25f5375391d69655983f8296b80273a6c
Daniel Romero <dandres508@gmail.com>
2024-09-23 14:15:10 -0500

    Tipos de objetos diferentes en la lista

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,32 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+import com.aluracursos.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evaluacion(9);
+        Pelicula miPelicula2 = new Pelicula("Matrix", 1998);
+        miPelicula2.evaluacion(6);
+        var peliculaDeBruno = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeBruno.evaluacion(10);
+        Serie casaDragon = new Serie("Casa del Dragon", 2023);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+
+        lista.add(miPelicula);
+        lista.add(miPelicula2);
+        lista.add(peliculaDeBruno);
+        lista.add(casaDragon);
+
+        for (Titulo item : lista) {
+            System.out.println(item);
+            Pelicula pelicula = (Pelicula) item;
+            System.out.println(pelicula.getClasificacion());
+        }
+    }
+}
