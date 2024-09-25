c03f5572f17393050035ca1e6e25df2ad6885fd8
Daniel Romero <dandres508@gmail.com>
2024-09-25 15:36:42 -0500

    Refinando la busqueda por pelicula

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -5,14 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Exriba el nombre de la pelicula: ");
+        var busqueda = teclado.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=1a5f7624";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=1a5f7624"))
+                .uri(URI.create(direccion))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
