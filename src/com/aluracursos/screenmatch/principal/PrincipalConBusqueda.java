1bd556037f2a26b23576974071fb461dbc817919
Daniel Romero <dandres508@gmail.com>
2024-09-26 15:42:19 -0500

    Grabando datos en un archivo

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -43,6 +44,9 @@ public class PrincipalConBusqueda {
 
             Titulo miTitulo = new Titulo(miTituloOmdb);
             System.out.println("Titulo ya convertido: " + miTitulo);
+            FileWriter  escritura = new FileWriter("peliculas.txt");
+            escritura.write(miTitulo.toString());
+            escritura.close();
         } catch (NumberFormatException e) {
             System.out.println("Ocurrió un error: " + e.getMessage());
         } catch (IllegalArgumentException e) {
